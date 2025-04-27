106d9236cc3513045b48dd5d5698610d2d441ec5
Vaibhav262610 <branda56235@gmail.com>
2025-04-28 00:51:40 +0530

    Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${mona_san.className} antialiased`}
+        className={`${mona_san.className} antialiased pattern`}
       >
         {children}
       </body>
